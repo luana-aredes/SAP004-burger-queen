4801800cf3ee6bebe74ddee89f4e063ea0521612
N. Monalisa <nathaliamonalisa@gmail.com>
2020-07-20 19:12:56 -0300

    Login implementado com mensagens de erros de autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import fire from '../src/config/firebase'
+import LoginPage from '../src/Pages/Login/index'
 
 function App() {
   return ( <
-    p > Ola! < /p>
+    >
+    <
+    p > Ola! < /p> <
+    LoginPage / >
+    <
+    />
 
   );
 }
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Input = (props) => {
-    return (
-        <input type={props.type} className={props.class} placeholder={props.placeholder} />
-    );
+  return ( <
+    input type = { props.type }
+    className = { props.class }
+    placeholder = { props.placeholder }
+    value = { props.value }
+    onChange = { props.onChange }
+    />
+  );
 }
 
 export default Input;
