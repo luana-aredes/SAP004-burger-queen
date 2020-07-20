7c49bab71bf5777d580acac87705904d8711e199
Luana Arêdes Costa <luanaredes@hotmail.com>
2020-07-20 10:51:11 -0300

    Config firebase

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import fire from '../src/config/firebase'
+function App() {
+  return (
+    <p>Ola!</p>
+
+  );
+}
+
+export default App;
---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomePage from '../HomePage'
+import Login from '../Login'
+import Register from '../Register'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
+
