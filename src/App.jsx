162eae6e1a0cd97f70903ad9a2e3bc0b447ec22b
N. Monalisa <nathaliamonalisa@gmail.com>
2020-07-20 15:21:56 -0300

    Pull atualizações do repositorio remoto

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import fire from '../src/config/firebase'
 
 function App() {
   return ( <
     p > Ola! < /p>
 
-=======
-import firebase from '../src/config/firebase';
-import Button from './Components/Button/Button'
-import Input from './Components/Input/Input'
-const submit = () => {
-  console.log('Registrei')
-}
-
-function App() {
-  return (
-    <form>
-      <Input class='input-register' type='e-mail' placeholder='Email' />
-      <Button handleCLick={submit} class='submit' name='Registrar' />
-    </form>
->>>>>>> f910a622fc0a8bf3473de9fc530536414bc2b288
   );
 }
 
